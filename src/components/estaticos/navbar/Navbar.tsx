4d8928f68d29e3b829b9aed1451d7faa6d4f6a47
Guzius <gustavotaques11@gmail.com>
2021-11-12 14:51:42 -0300

    Implementando Sistema de Autenticação

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -5,8 +5,9 @@ import { useHistory } from 'react-router-dom';
 import './Navbar.css';
 import { useSelector } from 'react-redux';
 import { TokenState } from '../../../store/tokens/tokensReducer';
-import { useDispatch } from "react-redux";
+import { useDispatch } from 'react-redux';
 import { addToken } from '../../../store/tokens/actions';
+import { toast } from 'react-toastify';
 
 function Navbar() {
     let history = useHistory();
@@ -17,7 +18,16 @@ function Navbar() {
 
       function goLogout(){
         dispatch(addToken(''));
-        alert("Usuário deslogado")
+        toast.info('Usuário deslogado', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "colored",
+            progress: undefined,
+        });
         history.push('/login')
     }
     var navbarComponent;
